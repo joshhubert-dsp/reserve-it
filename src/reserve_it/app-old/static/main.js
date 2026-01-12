5d0f19c86db475092cc8909fe41228fd4afe41e1
joshhubert-dsp <102703352+joshhubert-dsp@users.noreply.github.com>
2026-01-11 19:32:48 -0800

    jettisoning old

--- src/reserve_it/app-old/static/main.js
+++
@@ -1,8 +0,0 @@
-//  Dark mode toggle logic
-const themeButton = document.getElementById("theme-toggle");
-
-themeButton.addEventListener("click", () => {
-    const root = document.documentElement;
-    const isDark = root.classList.toggle("dark");
-    localStorage.setItem("theme", isDark ? "dark" : "light");
-});
